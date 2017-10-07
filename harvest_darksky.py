2de192718ddf73f602787b4ef68c156de4cd1dd7
ghidra <ghidra@gmail.com>
2017-10-06 23:58:25 -0400

    with lat and lon

--- harvest_darksky.py
+++ harvest_darksky.py
@@ -2,24 +2,42 @@ import urllib, json
 import math
 
 online = False
+####################################
 key = ""
-lat = 29.7604
 lon = 95.3698
-#
+lat = 29.7604
+maxDegree = 1.0 #about 69 miles 111 kilometers
+width = 16
+height = 10
 #lat is north and west. higher vales are more north. 0.001 is like a avenue
 #lon is east and south. higher values are west.
 #i need to get an aspect ratio to determine our grid
 #get width samples and height samples
 #as well as the max degrees will fit the largest w/h dime
-maxDegree = 1.0 #about 69 miles 111 kilometers
-width = 18
-height = 10
+####################################
+
+aspect = height/float(width)
+
+#starting point in lat lon
+x = lon+(maxDegree*0.5)
+y = lat+(maxDegree*aspect*0.5)
+
+#scale between each node to fit max degrees of map in
+sx = 1/float(width)
+sy = 1/float(height)
 
 #time to do some more grids, how many times do i have to do this
 for i in range(0,width*height):
 	row = math.floor(i/width)
-	col = i%width
-	print str(row)+":"+str(col)
+	col = float(i%width)
+
+	stepx = sx*col
+	stepy = sy*row
+
+	sample_lon = round((x-stepx)*10000.0)/10000.0
+	sample_lat = round((y-stepy)*10000.0)/10000.0
+	
+	print str(row)+":"+str(col)+"_"+str(sample_lat)+":"+str(sample_lon)
 	if col == width-1:
 		print '\n'
 
